9be28a28b4983eb594eb53caa67590ddd3a3a049
dshmif <dshmif@gmail.com>
2013-05-02 10:28:39 -0400

    Refactored most of the isSudoer method groups.

--- src/main/java/com/abstractthis/consoul/ApplicationContext.java
+++ src/main/java/com/abstractthis/consoul/ApplicationContext.java
@@ -22,9 +22,11 @@ package com.abstractthis.consoul;
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //THE SOFTWARE.
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.abstractthis.consoul.commands.CommandCreds;
 import com.abstractthis.consoul.ini.ConsoleInitializer;
 import com.abstractthis.consoul.util.BCrypt;
 
@@ -50,40 +52,50 @@ public final class ApplicationContext extends ConcurrentHashMap<String, ContextV
 		super(initialCapacity, loadFactor, concurrencyLevel);
 	}
 	
-	@SuppressWarnings("unchecked")
 	public boolean isSudoer() {
-		ContextVariable<Set<String>> sudoersCxtVar = (ContextVariable<Set<String>>) this.get(SUDOERS_CONTEXT_KEY);
-		Set<String> sudoersSet = sudoersCxtVar.getContent();
-		String hash = (String) this.get(CURRENT_SUDO_COMMAND).getContent();
-		return sudoersSet.contains(hash);
+		CommandCreds creds = this.removeCurrentSudoer();
+		return this.isSudoer(creds);
+	}
+	
+	public boolean isSudoer(CommandCreds creds) {
+		return this.isSudoer(creds.getCredsForHashing());
 	}
 	
 	public boolean isSudoer(String user, String password) {
 		return this.isSudoer(user + password);
 	}
 	
-	@SuppressWarnings("unchecked")
 	public boolean isSudoer(String concatCred) {
+		@SuppressWarnings("unchecked")
 		ContextVariable<Set<String>> sudoersCxtVar = (ContextVariable<Set<String>>) this.get(SUDOERS_CONTEXT_KEY);
 		Set<String> sudoersSet = sudoersCxtVar.getContent();
-		String hash = BCrypt.hashpw(concatCred, BCrypt.gensalt(12));
-		return sudoersSet.contains(hash);
-	}
-	
-	public void setCurrentSudoer(String hash) {
-		ContextVariable<?> creds = this.remove(CURRENT_SUDO_COMMAND);
-		if( creds != null ) {
-			creds.clear();
-			creds = null;
+		for(Iterator<String> i = sudoersSet.iterator(); i.hasNext();) {
+			String hash = i.next();
+			if( BCrypt.checkpw(concatCred, hash) ) {
+				return true;
+			}
 		}
-		creds = new ContextVariable<String>(hash);
-		this.put(CURRENT_SUDO_COMMAND, creds);
+		return false;
 	}
 	
-	@SuppressWarnings("unchecked")
-	public String removeCurrentSudoer() {
-		ContextVariable<String> sudoHash = (ContextVariable<String>) this.get(CURRENT_SUDO_COMMAND);
-		return sudoHash.getContent();
+	public void setCurrentSudoer(CommandCreds creds) {
+		@SuppressWarnings("unchecked")
+		ContextVariable<CommandCreds> ctxtCreds =
+				(ContextVariable<CommandCreds>) this.remove(CURRENT_SUDO_COMMAND);
+		if( ctxtCreds != null ) {
+			ctxtCreds.clear();
+			ctxtCreds = null;
+		}
+		ctxtCreds = new ContextVariable<CommandCreds>(creds);
+		this.put(CURRENT_SUDO_COMMAND, ctxtCreds);
+		System.err.println("Current Sudoer set");
+	}
+	
+	public CommandCreds removeCurrentSudoer() {
+		@SuppressWarnings("unchecked")
+		ContextVariable<CommandCreds> ctxtCreds =
+				(ContextVariable<CommandCreds>)this.remove(CURRENT_SUDO_COMMAND);
+		return ctxtCreds.getContent();
 	}
 	
 }
